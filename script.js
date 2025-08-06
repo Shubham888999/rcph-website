0c3b295c7fb477944ec8ff1c6d9e2054d42fd876
Shubham Deshpande <81420632+Shubham888999@users.noreply.github.com>
2025-08-06 23:43:55 +0530

    BOD Card updated

--- script.js
+++ script.js
@@ -22,13 +22,14 @@ function initCalendar() {
     initialView: 'dayGridMonth',
     height: 'auto',
     events: [
-      { title: 'The Blood Donation Camp', start: '2025-07-01', description: 'Blood Donation Drive conducted in collaboration with Rotary Club of Pune Heritage', time:'9:00 AM', venue:'TBA' },
-      { title: 'Charge Handover Ceremony', start: '2025-07-07', description:'We would be honoured to have your presence as we mark this important transition and begin a new chapter for RC Pune Heritage', time:'6:45 PM', venue:'Inari Bistro' },
-      { title: 'Ice Breaker', start: '2025-07-12', description:'From fun games to meaningful conversations', time:'4:00 PM', venue:'Tathawade baug' },
-      { title: 'Energy Within', start: '2025-07-20', description:'An expert session on Pranic Healing - a no-touch technique that uses life energy (prana) to cleanse and balance the body', time:'10:30 AM', venue:'Chandrakant Darode School' },
-      { title: 'GBM Meeting 1', start: '2025-07-18', description:'General Body Meeting 1', time:'7:45 PM', venue:'Inari Bistro' },
+      { title: 'The Blood Donation Camp', start: '2025-07-01', description: 'Blood Donation Drive conducted in collaboration with Rotary Club of Pune Heritage' },
+      { title: 'Charge Handover Ceremony', start: '2025-07-07', description:'We would be honoured to have your presence as we mark this important transition and begin a new chapter for RC Pune Heritage' },
+      { title: 'Ice Breaker', start: '2025-07-12', description:'From fun games to meaningful conversations' },
+      { title: 'Energy Within', start: '2025-07-20', description:'An expert session on Pranic Healing - a no-touch technique that uses life energy (prana) to cleanse and balance the body' },
+      { title: 'GBM Meeting 1', start: '2025-07-18', description:'General Body Meeting 1' },
       { title: 'Potluck Lunch (CSD)', start: '2025-08-03', description:'A fun bonding lunch with installation planning' },
       { title: 'Paw Trait (CMD)', start: '2025-08-04', end: '2025-08-10', description:'Feeding nutritious food to stray dogs' },
+      { title: 'Work In Progress', start: '2025-08-08', description:'No slides. No scripts. Just real stories about what we do, why we do it, and where we’re headed.Each person shares their journey and answers 2 fun career questions on the spot!' },
       { title: 'Bappa Making', start: '2025-08-23', description:'Learning the skills to make bappa along with fun' },
       { title: 'Samyati 3 (ISD)', start: '2025-08-29', end: '2025-09-01', description:'Details announcing soon' },
       { title: 'Sevasarthi (CMD)', start: '2025-09-14' },
@@ -43,8 +44,8 @@ function initCalendar() {
       document.getElementById('eventTitle').textContent       = info.event.title;
       document.getElementById('eventDescription').textContent = info.event.extendedProps.description;
       document.getElementById('eventDate').textContent        = info.event.start.toDateString();
-      document.getElementById('eventTime').textContent        = info.event.extendedProps.time;
-      document.getElementById('eventVenue').textContent       = info.event.extendedProps.venue;
+      //document.getElementById('eventTime').textContent        = info.event.extendedProps.time;
+      //document.getElementById('eventVenue').textContent       = info.event.extendedProps.venue;
       document.getElementById('eventModal').style.display     = 'block';
     }
   });
